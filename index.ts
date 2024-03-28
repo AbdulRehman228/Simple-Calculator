6020f21b1eac755d1120fc44954e418c58846389
AbdulRehman228 <syedabdulrahman996@gmail.com>
2024-03-28 10:12:26 -0700

    Adding a simple cli calculator

---
+++ index.ts
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// priniting a Wellcome Message
+console.log("\n\tWellcome To \'ABdul Rehman'\ - CLI simple calculator\n\t");
+
+
+// Asking Questions from users through Inquirer
+
+let answers = await inquirer.prompt([
+    {message: "Enter First Number", type:"number", name: "firstNumber"},
+    {message: "Enter Second Number", type:"number", name: "secondNumber"},
+    {
+        message: "Select One Operator to perform Operator",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtractions", "Multiplication", "Division"],
+    },
+]);
+
+// conditional statement If-Else
+
+if(answers.operator === "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber)
+}
+
+else if (answers.operator === "Subtractions"){
+    console.log(answers.firstNumber - answers.secondNumber)
+}
+
+else if (answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+}
+
+else if (answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber)
+}
+else {
+    console.log("Invalid Input")
+}
+
